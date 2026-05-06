1b478a73d3cfbddd9fd9146c4f2521ddda8a3e0f
the-pro-crastinator <indomitable6174@gmail.com>
2026-05-06 20:08:50 +0530

    Create 02_linear_search.java

---
+++ Basics/02_linear_search.java
@@ -0,0 +1,28 @@
+// Linear Search
+// Best Case: O(1)
+// Worst Case: O(n)
+// Space Complexity: O(1)
+
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+// arr = [2, 5, 7, 1]
+// target = 7
+
+class Main {
+    public static void main(String[] args) {
+        int[] arr = {2, 5, 7, 1};
+        int target = 7;
+        boolean flag = false;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]==target){
+                System.out.println("Found at index " + i);
+                flag = true;
+                break;
+            }
+        }
+        if(!flag){
+            System.out.println("-1");
+        }
+        // remember flags (very important)
+    }
+}
